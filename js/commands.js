5cba68a61d27843a3e664314ec6faf866c05ea7b
Paul Dorney <pdorney@gmail.com>
2024-02-15 21:18:09 -0500

    intial commit

---
+++ js/commands.js
@@ -0,0 +1,80 @@
+var linkedin = "https://www.linkedin.com/in/pauldorney/";
+var twitter = "https://twitter.com/jetrased";
+var password = "jetrased";
+var github = "https://github.com/pdorney";
+var email = 'mailto:pdorney@gmail.com';
+
+whois = [
+"<br>",
+"Hi, I'm Paul! 🏴‍☠️",
+"I'm a software developer, founder and startup advisor 🎲,",
+"After graduating from UCC in computer science and spending a year in Galway (Highly recommend) - I moved to the US to continue my career.",
+"After stops at the large corporations of Fidelity Investments & SAS Institute, I decided to take the plunge and found my own company in 2016.",
+"Silbo was a two-sided marketplace for sports officials - think 'Uber Of Sports Officials'",
+"We successfully exited in 2020 to Comcast/NBC after many ups, downs, and lots of learning.",
+"The next adventure had me take over the technology team at PunchlistUSA (Rebranded to BOSSCAT).",
+"During my 3 years as CTO - We revamped the entire company from top to bottom, raised a $39m series A,",
+"won plenty of company awards, and skyrocketed revenue all while stabilizing the current technology stack and building an entirely new platform.",
+"<br>"
+];
+
+whoami = [
+  "<br>",
+  "In the vast, indifferent cosmos, you're a remarkable, towel-carrying hitchhiker, ",
+  "navigating the galaxy with a peculiar blend of optimism and a critical need for tea, all while knowing the importance of the number 42.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/pauldorney' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/jetrased' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/pdorney' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+list = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Paul?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">list</span>           List the commands',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Welcome to Jetrased.com - Paul Dorney.</span>',
+  ".------..------..------..------.     .------..------..------..------..------..------.",
+  "|P.--. ||A.--. ||U.--. ||L.--. |.-.  |D.--. ||O.--. ||R.--. ||N.--. ||E.--. ||Y.--. |",
+  "| :/\: || (\/) || (\/) || :/\: ((5)) | :/\: || :/\: || :(): || :(): || (\/) || (\/) |",
+  "| (__) || :\/: || :\/: || (__) |'-.-.| (__) || :\/: || ()() || ()() || :\/: || :\/: |",
+  "| '--'P|| '--'A|| '--'U|| '--'L| ((1)) '--'D|| '--'O|| '--'R|| '--'N|| '--'E|| '--'Y|",
+  "`------'`------'`------'`------'  '-'`------'`------'`------'`------'`------'`------'",
+
+  
+  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to the Terminal, use the commands to learn more.</span>',
+  "<span class=\"color2\">For a list of available commands, give</span> <span class=\"command\">'list'</span><span class=\"color2\"> a whirl.</span>",
+];
+
+
+
+                                                                                                    
